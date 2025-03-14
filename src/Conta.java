e3ad55dcac95aa27815e79d83fd3bdf994ea8171
vgonsalez <gonsalez@yahoo.com>
2025-03-14 19:06:02 -0300

    implementacao da interface iconta

--- src/Conta.java
+++ src/Conta.java
@@ -1,15 +1,9 @@
 
-public class Conta {
+public class Conta implements IConta {
 	
 	private int agencia;
 	private int numero;
 	private double saldo;
-	
-	public void sacar() {}
-	
-	public void depositar() {} 
-	
-	public void transferir() {}
 	
 	public int getAgencia() {
 		return agencia;
@@ -19,5 +13,21 @@ public class Conta {
 	}
 	public double getSaldo() {
 		return saldo;
+	}
+	
+	@Override
+	public void sacar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void depositar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void transferir(double valor, Conta contaDestino) {
+		// TODO Auto-generated method stub
+		
 	}
 }
