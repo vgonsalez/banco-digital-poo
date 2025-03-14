cc89049cb03c3bae4cbfddb32b7f30e70ca349a3
vgonsalez <gonsalez@yahoo.com>
2025-03-14 17:24:49 -0300

    adicao de metodos nas classes conta corrente e conta poupanca

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -3,4 +3,9 @@ public class ContaPoupanca {
 	private int agencia;
 	private int numero;
 	private double saldo;
+	
+	public void sacar() {}
+	public void depositar() {}
+	public void transferir() {}
+
 }
